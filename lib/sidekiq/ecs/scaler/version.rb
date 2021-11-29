96ab6051b3ef0f009115b6bc3f0b0b3b986334e6
shoma07 <23730734+shoma07@users.noreply.github.com>
2021-11-29 23:33:16 +0900

    initial commit

---
+++ lib/sidekiq/ecs/scaler/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Sidekiq
+  module Ecs
+    module Scaler
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ spec/sidekiq/ecs/scaler_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Sidekiq::Ecs::Scaler do
+  it "has a version number" do
+    expect(Sidekiq::Ecs::Scaler::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
